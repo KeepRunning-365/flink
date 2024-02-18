380ec9086396ad1406b08d20e3be7c6608b47d70
Changshuai Zhu <1163182716@qq.com>
2024-02-18 19:47:18 +0800

    初次尝试
    自定义序列化器

---
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/myself/FurySerializer.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.examples.myself;
+
+import org.apache.flink.streaming.examples.socket.SocketWindowWordCount;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import io.fury.Fury;
+import io.fury.ThreadSafeFury;
+import io.fury.config.CompatibleMode;
+import io.fury.config.Language;
+
+import java.io.Serializable;
+
+/**
+ * 自定义序列化器.
+ */
+public class FurySerializer extends Serializer<SocketWindowWordCount.WordWithCount> implements Serializable {
+
+    // 建议作为一个全局变量，避免重复创建
+    final Fury fury = Fury.builder()
+            .withLanguage(Language.JAVA)
+            //开启共享引用/循环引用支持，不需要的话建议关闭，性能更快
+            .withRefTracking(true)
+            // 允许序列化未注册类型
+            // .withClassRegistrationRequired(false)
+            // 开启int/long压缩，减少序列化数据大小，无该类需求建议关闭，性能更好
+            // .withNumberCompressed(true)
+            .withCompatibleMode(CompatibleMode.SCHEMA_CONSISTENT)
+            // 开启类型前后兼容，允许序列化和反序列化字段不一致，无该类需求建议关闭，性能更好
+            // .withCompatibleMode(CompatibleMode.COMPATIBLE)
+            // 开启异步多线程编译
+            .withAsyncCompilation(true)
+            .requireClassRegistration(false)
+            .build();
+
+    ThreadSafeFury threadSafeFury = Fury.builder()
+            .withLanguage(Language.JAVA)
+            //开启共享引用/循环引用支持，不需要的话建议关闭，性能更快
+            .withRefTracking(true)
+            // 允许序列化未注册类型
+            // .withClassRegistrationRequired(false)
+            // 开启int/long压缩，减少序列化数据大小，无该类需求建议关闭，性能更好
+            // .withNumberCompressed(true)
+            .withCompatibleMode(CompatibleMode.SCHEMA_CONSISTENT)
+            // 开启类型前后兼容，允许序列化和反序列化字段不一致，无该类需求建议关闭，性能更好
+            // .withCompatibleMode(CompatibleMode.COMPATIBLE)
+            // 开启异步多线程编译
+            .withAsyncCompilation(true)
+            .buildThreadSafeFury();
+
+    @Override
+    public void write(Kryo kryo, Output output, SocketWindowWordCount.WordWithCount object) {
+        byte[] bytes = fury.serialize(object);
+        System.out.println("自定义序列化 - 写："+bytes.length);
+        output.writeInt(bytes.length,true);
+        output.writeBytes(bytes);
+    }
+
+    @Override
+    public SocketWindowWordCount.WordWithCount read(
+            Kryo kryo,
+            Input input,
+            Class<SocketWindowWordCount.WordWithCount> type) {
+        int read = input.readInt(true);
+        System.out.println("自定义序列化 - 读："+read );
+        byte[] barr = new byte[read];
+        input.readBytes(barr);
+        return (SocketWindowWordCount.WordWithCount) fury.deserialize(barr);
+    }
+}
--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/socket/SocketWindowWordCount.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/socket/SocketWindowWordCount.java
@@ -18,12 +18,19 @@
 
 package org.apache.flink.streaming.examples.socket;
 
+import com.esotericsoftware.kryo.Serializer;
+
+import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.PipelineOptions;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.examples.myself.FurySerializer;
 
 import java.time.Duration;
 
@@ -62,8 +69,19 @@ public class SocketWindowWordCount {
         }
 
         // get the execution environment
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        Configuration conf = new Configuration();
 
+//        conf.setString("taskmanager.memory.managed.size","0m");
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(
+                conf);
+        // 禁用 kryo 进行序列化，效率低
+//        env.getConfig().disableGenericTypes();
+        env.getConfig().registerTypeWithKryoSerializer(WordWithCount.class, FurySerializer.class);
+        // 打印类使用的序列化方式
+        System.out.println("序列化方式：" + TypeInformation
+                .of(WordWithCount.class)
+                .createSerializer(new ExecutionConfig()));
         // get input data by connecting to the socket
         DataStream<String> text = env.socketTextStream(hostname, port, "\n");
 
@@ -76,7 +94,8 @@ public class SocketWindowWordCount {
                                                 out.collect(new WordWithCount(word, 1L));
                                             }
                                         },
-                                Types.POJO(WordWithCount.class))
+                                TypeInformation.of(WordWithCount.class)
+                        )
                         .keyBy(value -> value.word)
                         .window(TumblingProcessingTimeWindows.of(Duration.ofSeconds(5)))
                         .reduce((a, b) -> new WordWithCount(a.word, a.count + b.count))
@@ -97,7 +116,8 @@ public class SocketWindowWordCount {
         public long count;
 
         @SuppressWarnings("unused")
-        public WordWithCount() {}
+//        public WordWithCount() {
+//        }
 
         public WordWithCount(String word, long count) {
             this.word = word;
